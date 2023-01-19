30554a20fd71f8d992b56fd983b8c065b2309a21
green ninja <greeninjawillbewebprogrammer@gmail.com>
2023-01-19 20:23:14 +0500

    test

---
+++ lets go/script.js
@@ -0,0 +1,29 @@
+var one = prompt(' Question 1/15:  Whats your name? ')
+
+var two = prompt(' Question 2/15:  How old are you? ')
+
+var three = prompt('Question 3/15:  Are you single? ')
+
+var four = prompt('Question 4/15:  Do you have a girlfriend? ')
+
+var five = prompt('Question 5/15:  How many friends do you have? ')
+
+var six = prompt('Question 6/15:  77+33=? ')
+
+var seven = prompt('Question 7/15:  Which city do you like? ')
+
+var eight = prompt('Question 8/15:  Which car do you like? ')
+
+var nine = prompt('Question 9/15:  What do you like to do at the weekends? ')
+
+var ten = prompt('Question 10/15:  Do you sleep enough? ')
+
+var eleven = prompt('Question 11/15:  Which food do you prefer? ')
+
+var twelve = prompt('Question 12/15:  Do you like play computer games? ')
+
+var thirsteen = prompt('Question 13/15:  Do you like watching movie? ')
+
+var fourteen = prompt('Question 14/15:  How often do you online? ')
+
+var fiveteen = prompt('Question 15/15:  Which messenger do you like? ')
